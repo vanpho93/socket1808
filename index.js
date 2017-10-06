86f278aceb25f8bed44b5e3b5a2522fe5d9c62af
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-06 20:08:55 +0700

    add socket.io

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const reload = require('reload');
+
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -10,4 +13,4 @@ app.use(express.static('./public'));
 app.get('/', (req, res) => res.render('home'));
 
 reload(app);
-app.listen(3000, () => console.log('Server start!'));
+server.listen(3000, () => console.log('Server start!'));
