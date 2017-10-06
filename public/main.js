4ddf5527eff7bf27d11496f0fec32cc9cb71660e
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-06 20:59:57 +0700

    xong server

--- public/main.js
+++ public/main.js
@@ -4,7 +4,14 @@ socket.on('SERVER_SEND_MESSAGE', message => {
     $('#divMessages').append(`<p>${message}</p>`)
 });
 
-$('#btnSend').click(() => {
+// $('#btnSend').click(() => {
+//     const message = $('#txtMessage').val();
+//     socket.emit('CLIENT_SEND_MESSAGE', message);
+//     $('#txtMessage').val('');
+// });
+
+$('#formChat').submit((e) => {
+    e.preventDefault();
     const message = $('#txtMessage').val();
     socket.emit('CLIENT_SEND_MESSAGE', message);
     $('#txtMessage').val('');
